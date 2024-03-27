e12a1c21d1177bf2ddc6e285b9f650fa56926748
Pradeep Kumar <pradeepinform11@gmail.com>
2024-03-27 20:04:14 +0530

    basicecode java

--- basicecode/FirstProgram.java
+++ basicecode/FirstProgram.java
@@ -1,7 +1,10 @@
+package basicecode;
+
 public class FirstProgram {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("My name is pradeep kumar");
     }
 }
